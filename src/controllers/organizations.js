b7454e366e3c331456ccdad7d738d47b53ba5d48
sosath <sosalijeronthomas@gmail.com>
2026-05-29 17:14:59 -0400

    Add organization details feature: Implement controller, model, and view for displaying organization details and associated projects

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -1,5 +1,6 @@
 // Import any needed model functions
-import { getAllOrganizations } from '../models/organizations.js';
+import { getAllOrganizations, getOrganizationDetails } from '../models/organizations.js';
+import { getProjectsByOrganizationId } from '../models/projects.js';
 
 // Define any controller functions
 const showOrganizationsPage = async (req, res) => {
@@ -9,5 +10,14 @@ const showOrganizationsPage = async (req, res) => {
     res.render('organizations', { title, organizations, pageTitle: title });
 };
 
+const showOrganizationDetailsPage = async (req, res) => {
+    const organizationId = req.params.id; // Capturamos el parámetro de ruta ':id'
+    const organizationDetails = await getOrganizationDetails(organizationId);
+    const projects = await getProjectsByOrganizationId(organizationId);
+    const title = 'Organization Details';
+
+    res.render('organization', { title, organizationDetails, projects, pageTitle: title });
+};
+
 // Export any controller functions
-export { showOrganizationsPage };
+export { showOrganizationsPage, showOrganizationDetailsPage };
